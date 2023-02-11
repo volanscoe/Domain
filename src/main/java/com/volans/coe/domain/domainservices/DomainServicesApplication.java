e05d2d953cae9ff251e750a53362c85406717942
Saurabh Saxena <saurabh@saurabhs-mbp.myfiosgateway.com>
2023-02-11 16:25:47 -0500

    Adding basic skeleton files

---
+++ src/main/java/com/volans/coe/domain/domainservices/DomainServicesApplication.java
@@ -0,0 +1,13 @@
+package com.volans.coe.domain.domainservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainServicesApplication.class, args);
+	}
+
+}
